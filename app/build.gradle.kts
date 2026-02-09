1681bbc049f5d2b5b77b37d9de6ae4b8cd21a39f
KEVIN <munenekevin366@gmail.com>
2026-02-09 15:17:53 +0300

    v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kevannTechnologies.nosteqTech"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
